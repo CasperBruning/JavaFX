f9f89462b632088d3212858411b586842ca4d6ca
MagicGnome-UwU <casperveneman25@gmail.com>
2022-03-10 10:59:09 +0100

    added FXH4.6

--- Opdracht1/FXH4.6/src/Dobbelsteen.java
+++ Opdracht1/FXH4.6/src/Dobbelsteen.java
@@ -3,11 +3,23 @@ import java.util.Random;
 public class Dobbelsteen
 {
     Random rand = new Random();
-
+    int randomNumber = rand.nextInt(6) + 1;
 
     public int returngooi()
     {
-        return rand.nextInt(6) + 1;
-
+        return randomNumber;
     }
+
+    public int checkSix()
+    {
+        int thrownSix = 0;
+
+        if (randomNumber==6)
+        {
+            thrownSix += 1;
+        }
+        
+        return thrownSix;
+    }
+
 }
